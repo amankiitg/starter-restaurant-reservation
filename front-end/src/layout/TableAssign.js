da0ed9a377c8de648531bb40cd1dbcc34d0bcf99
Aman Kesarwani <amank.iitg@gmail.com>
2022-08-31 22:35:17 -0400

    US04-Frontend Passed need to fix warnings

--- front-end/src/layout/TableAssign.js
+++ front-end/src/layout/TableAssign.js
@@ -18,7 +18,7 @@ function TableAssign() {
 
         async function loadTables() {
             const abortController = new AbortController();
-            
+    
             listTables({}, abortController.signal)
                 .then(setTables)  
             return () => abortController.abort();
